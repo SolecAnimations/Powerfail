53ac749dfb12e5ef705f049852139b25ef5e293a
SputnikSoyuz <bigfatsingularity@proton.me>
2026-07-03 15:59:17 -0400

    Power Tower Complete + More
    - Lights and atmosphere now turn off when inside the Power Tower's tip.
    - Nomai Orb Switch complete! Includes electric barriers in the maze and the Lightbulb can turn off properly.
    - All props on the Lightbulb are now placed in a decent spot.
    - Some props on Wilds Refuge are now placed in a decent spot, including the Inhabitant village.
    - Fixed positioning of the projection pool in the Asteroid.
    - Fixed wrong name in Cherry's dialogue.
    - Fixed Shiplog icon for Bruised Brother
    - Various other prop placings and fixes.

---
+++ Powerfail/BrotherLights.cs
@@ -0,0 +1,38 @@
+﻿using NewHorizons.Utility;
+using UnityEngine;
+
+namespace PowerFail;
+
+public class BrotherLights : MonoBehaviour
+{
+    private GameObject[] objectsToDisable = new GameObject[2];
+
+    public void Start()
+    {
+        objectsToDisable[0] = SearchUtilities.Find("BruisedBrother_Body/Sector").transform.GetChild(0).gameObject;
+        objectsToDisable[1] = SearchUtilities.Find("BruisedBrother_Body/Sector/Atmosphere/Atmosphere");
+    }
+    public void ToggleLights()
+    {
+        foreach (GameObject obj in objectsToDisable)
+        {
+            if (obj.activeSelf)
+            {
+                obj.SetActive(false);
+            } else
+            {
+                obj.SetActive(true);
+            }
+            
+        }
+    }
+
+    public virtual void OnTriggerEnter(Collider hitCollider)
+    {
+        //checks if player collides with the trigger volume
+        if (hitCollider.CompareTag("PlayerDetector") && enabled)
+        {
+            ToggleLights();
+        }
+    }
+}
